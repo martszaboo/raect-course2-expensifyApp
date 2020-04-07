ec5c4aeb7a90619b8a53a10fa9742b9cf2efcb72
Szabó Márton <marton.szaboo@gmail.com>
2020-04-07 22:56:53 +0200

    setup prod webpack build

---
+++ src/Readme.md
@@ -0,0 +1,7 @@
+# git Commands
+
+git init - create a new it repo
+git status - View the changes to your project code
+git add - add files to your staging area
+git commit - creates a new commit with files from staging area
+git log - view recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 
+const ExtractTextPlugin =require('extract-text-webpack-plugin')
 const path = require('path')
 
+module.exports=(env)=>{
+    const CSSExtract = new ExtractTextPlugin('styles.css')
+    const isProduction= env==='production';
 
 
-module.exports ={
+    return{
     entry: './src/app.js',
     output:{
         path:path.join(__dirname,'public'),
@@ -17,17 +21,33 @@ module.exports ={
         },
     {
         test: /\.s?css$/,
-        use : [
-            'style-loader',
-            'css-loader',
-            'sass-loader'
-        ]
+        use : CSSExtract.extract({
+            use:[
+                {
+                loader:'css-loader',
+                options:{
+                    sourceMap:true
+                }
+            },
+                {loader:'sass-loader',
+                options:{
+                    sourceMap:true
+                }
+            }
+            ]
+        })
     }]
        
     },
-    devtool:'cheap-module-eval-source-map',
+    plugins:[
+        CSSExtract
+    ],
+
+    devtool: isProduction ?'source-map':'inline-source-map',
     devServer:{
         contentBase: path.join(__dirname,'public'),
         historyApiFallback: true
     }
+    }
 }
+
